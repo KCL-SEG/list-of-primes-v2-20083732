c0c6fbbc9b5d395c89ff8ab0d54e645c1acf1ceb
Xianlong Zhang <k20083732@kcl.ac.uk>
2022-10-13 10:23:59 +0100

    DONE WITH SGT

--- primes.py
+++ primes.py
@@ -1,6 +1,24 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+
 def primes(number_of_primes):
     list = []
+    iterator = 0
+
+
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes is not to be set to 0.")
+
+    if number_of_primes>=1:
+        for i in range (1, 1000000000000000000):
+            for j in range(1, i+1):
+                if(i%j == 0):
+                    iterator += 1
+            if(iterator == 2):
+                list.append(i)
+            iterator = 0
+            if len(list)==number_of_primes:
+                break
     return list
